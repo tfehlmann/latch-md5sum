fb891d2a11bfaf67204f8ace17187ae3b64141f6
Tobias Fehlmann <tobias@astera.org>
2022-09-23 16:24:51 -0700

    Use a thread to download file to pipe

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,8 @@ FROM 812206152185.dkr.ecr.us-west-2.amazonaws.com/latch-base:6839-main
 # STOP HERE:
 # The following lines are needed to ensure your build environement works
 # correctly with latch.
-RUN python3 -m pip install --upgrade latch
 COPY wf /root/wf
+RUN python3 -m pip install --upgrade latch
 ARG tag
 ENV FLYTE_INTERNAL_IMAGE $tag
 WORKDIR /root
--- wf/__init__.py
+++ wf/__init__.py
@@ -4,10 +4,13 @@ Compute the md5 checksum of a file.
 
 import hashlib
 import logging
+import os
 import subprocess
 import tempfile
+import threading
 from typing import cast
 
+from flytekit.extras.persistence.latch import LatchPersistence
 from latch import custom_task, workflow
 from latch.resources.launch_plan import LaunchPlan
 from latch.types import (
@@ -24,6 +27,14 @@ CHUNK_SIZE = 4096
 UPDATE_INTERVAL = int(500 * 1024 * 1024 / CHUNK_SIZE)  # all 500 MB
 
 
+def download_file(remote_or_local_path: str, local_path: str, remote: bool) -> None:
+    """Download a file from a remote or local location to a local path."""
+    if remote:
+        LatchPersistence().download(remote_or_local_path, local_path)
+    else:
+        subprocess.run(["cp", remote_or_local_path, local_path], check=True)
+
+
 @custom_task(cpu=1, memory=1)
 def compute_md5sum_task(file: LatchFile, output_file: LatchFile) -> LatchFile:
     """
@@ -32,26 +43,28 @@ def compute_md5sum_task(file: LatchFile, output_file: LatchFile) -> LatchFile:
     with tempfile.TemporaryDirectory() as tmp_dir:
         file_pipe = os.path.join(tmp_dir, "fifo")
         os.mkfifo(file_pipe)
-        if file.remote_path is not None:
-            file_source = file.remote_path
-            cp_cmd = ["latch", "cp"]
-        else:
-            file_source = file.local_path
-            cp_cmd = ["cp"]
-        with subprocess.Popen([*cp_cmd, file_source, file_pipe]) as cp_proc:
-            md5sum = hashlib.md5()
-            with open(file_pipe, "rb") as f:
-                for i, chunk in enumerate(iter(lambda: cast(bytes, f.read(CHUNK_SIZE)), b"")):
-                    if i > 0 and i % UPDATE_INTERVAL == 0:
-                        mb_read = i * CHUNK_SIZE / 1024 / 1024
-                        logging.info("Read %.2f MB", mb_read)
-                    md5sum.update(chunk)
-            result = md5sum.hexdigest()
-            with open("/tmp/md5sum.txt", "w", encoding="UTF-8") as f:
-                f.write(f"{result}\t{file.remote_path}\n")
+        download_thread = threading.Thread(
+            target=download_file,
+            args=(
+                file.remote_path if file.remote_path else file.local_path,
+                file_pipe,
+                file.remote_path is not None,
+            ),
+        )
+        download_thread.start()
 
-            if cp_proc.wait() != 0:
-                raise RuntimeError("latch cp failed")
+        md5sum = hashlib.md5()
+        with open(file_pipe, "rb") as f:
+            for i, chunk in enumerate(iter(lambda: cast(bytes, f.read(CHUNK_SIZE)), b"")):
+                if i > 0 and i % UPDATE_INTERVAL == 0:
+                    mb_read = i * CHUNK_SIZE / 1024 / 1024
+                    logging.info("Read %.2f MB", mb_read)
+                md5sum.update(chunk)
+        result = md5sum.hexdigest()
+        with open("/tmp/md5sum.txt", "w", encoding="UTF-8") as f:
+            f.write(f"{result}\t{file.remote_path}\n")
+
+        download_thread.join()
         return LatchFile("/tmp/md5sum.txt", remote_path=output_file.remote_path)  # type: ignore
 
 
@@ -98,8 +111,6 @@ LaunchPlan(
 )
 
 if __name__ == "__main__":
-    import os
-
     project_dir = os.path.dirname(os.path.dirname(__file__))
     compute_md5sum(
         file=LatchFile(os.path.join(project_dir, "data", "test_file.txt")),  # type: ignore
